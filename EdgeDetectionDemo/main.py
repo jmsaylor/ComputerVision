d86d7a44115ae23e36a70d7645caca8af8cea290
jmsaylor <johnmsaylor@gmail.com>
2021-02-15 10:52:08 -0500

    Harris and Shi-Tomasi Corner Detection

--- EdgeDetectionDemo/main.py
+++ EdgeDetectionDemo/main.py
@@ -8,10 +8,85 @@ if __name__ == '__main__':
     # img = cv2.imread('/home/jm/Pictures/fibers.jpg')
     img = cv2.imread('/home/jm/Pictures/schwartz.jpg')
 
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # img = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
 
     # img = cv2.blur(img, ksize=(17, 17))
+
+    # Shi-Tomasi Corner Detection
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    img_gray = cv2.blur(img_gray, ksize=(9, 9))
+
+    corners = cv2.goodFeaturesToTrack(img_gray, maxCorners=100, qualityLevel=.01, minDistance=20)
+
+    img_2 = img.copy()
+
+    for i in corners:
+        x, y = i.ravel()
+        cv2.circle(img_2, center=(x, y), radius=5, color=(0, 255, 0), thickness=-1)
+
+    plt.figure(figsize=(20, 20))
+    plt.subplot(1,2,1); plt.imshow(img); plt.axis('off')
+    plt.subplot(1,2,2); plt.imshow(img_2); plt.axis('off')
+    plt.show()
+
+    # Harris Corner Detection
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    #
+    # dst = cv2.cornerHarris(img_gray, blockSize=2, ksize=3, k=.04)
+    #
+    # img_2 = img.copy()
+    #
+    # img_2[dst>0.01*dst.max()] = [0,255,0]
+    #
+    # plt.figure(figsize=(20,20))
+    # plt.subplot(1, 2, 1); plt.imshow(img); plt.axis('off')
+    # plt.subplot(1, 2, 2,); plt.imshow(img_2); plt.axis('off')
+    # plt.show()
+
+    # Canny Detection w/ blurring and different threshold techniques
+
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #
+    # med_val = np.median(img)
+    #
+    # lower = int(max(0, .7 * med_val))
+    # upper = int(min(255, 1.3 * med_val))
+    #
+    # img_k5 = cv2.blur(img, ksize=(5, 5))
+    #
+    # edges_k5 = cv2.Canny(img_k5, threshold1=lower, threshold2=upper)
+    # edges_k5_2 = cv2.Canny(img_k5, threshold1=lower, threshold2=(upper + 100))
+    #
+    # img_k9 = cv2.blur(img, ksize=(9,9))
+    #
+    # edges_k9 = cv2.Canny(img_k9, lower, upper)
+    # edges_k9_2 = cv2.Canny(img_k9, lower, upper + 100)
+    #
+    # images = [edges_k5, edges_k5_2, edges_k9, edges_k9_2]
+    #
+    # plt.figure(figsize=(20, 15))
+    # for i in range(4):
+    #     plt.subplot(2, 2, i + 1)
+    #     plt.imshow(images[i])
+    #     plt.axis('off')
+    # plt.show()
+
+    # Canny Detection
+
+    # edges = cv2.Canny(image=img, threshold1=127, threshold2=127)
+    #
+    # plt.figure(figsize=(20,20))
+    # plt.subplot(1, 2, 1)
+    # plt.imshow(img)
+    # plt.axis('off')
+    # plt.subplot(1,2,2)
+    # plt.imshow(edges)
+    # plt.axis('off')
+    # plt.show()
 
     # Open/Close (Dilation/Erosion)
     #
@@ -76,22 +151,22 @@ if __name__ == '__main__':
 
     # Adaptive thresholding
 
-    _, thresh_binary = cv2.threshold(img, thresh=127, maxval=255, type=cv2.THRESH_BINARY)
-
-    adap_mean_2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 2)
-    adap_mean_2_inv = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 7, 2)
-    adap_mean_8 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 8)
-    adap_gaussian_8 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 8)
-
-    images = [img, thresh_binary, adap_mean_2, adap_mean_2_inv, adap_mean_8, adap_gaussian_8]
-
-    fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(15, 15))
-
-    for ind, p in enumerate(images):
-        ax = axs[ind%2, ind//2]
-        ax.imshow(p, cmap='gray')
-        ax.axis('off')
-    plt.show()
+    # _, thresh_binary = cv2.threshold(img, thresh=127, maxval=255, type=cv2.THRESH_BINARY)
+    #
+    # adap_mean_2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 2)
+    # adap_mean_2_inv = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 7, 2)
+    # adap_mean_8 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 8)
+    # adap_gaussian_8 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 8)
+    #
+    # images = [img, thresh_binary, adap_mean_2, adap_mean_2_inv, adap_mean_8, adap_gaussian_8]
+    #
+    # fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(15, 15))
+    #
+    # for ind, p in enumerate(images):
+    #     ax = axs[ind%2, ind//2]
+    #     ax.imshow(p, cmap='gray')
+    #     ax.axis('off')
+    # plt.show()
 
     #Threshold Example
     # _, thresh_0 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
